8fa947b0bbd4f72a043efb4f44d3e3a118378f8a
BBIDBoston <dboston@bbilogistics.com>
2024-01-12 13:28:37 -0500

    Add test job

--- Workload.sh
+++ Workload.sh
@@ -6,20 +6,20 @@ applicationRegistrationObjectId=$(echo $applicationRegistrationDetails | jq -r '
 applicationRegistrationAppId=$(echo $applicationRegistrationDetails | jq -r '.appId')
 
 az ad app federated-credential create \
-   --id $applicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-test-boston\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Website\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+    --id $applicationRegistrationObjectId \
+    --parameters "{\"name\":\"toy-website-test-boston\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Website\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
 az ad app federated-credential create \
-   --id $applicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-test-boston-branch\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+    --id $applicationRegistrationObjectId \
+    --parameters "{\"name\":\"toy-website-test-boston-branch\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
 resourceGroupResourceId=$(az group create --name ToyWebsiteTestBoston --location westus3 --query id --output tsv)
 
 az ad sp create --id $applicationRegistrationObjectId
 az role assignment create \
-  --assignee $applicationRegistrationAppId \
-  --role Contributor \
-  --scope /$resourceGroupResourceId
+    --assignee $applicationRegistrationAppId \
+    --role Contributor \
+    --scope /$resourceGroupResourceId
 
 echo "AZURE_CLIENT_ID: $applicationRegistrationAppId"
 echo "AZURE_TENANT_ID: $(az account show --query tenantId --output tsv)"
